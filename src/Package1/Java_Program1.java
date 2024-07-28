c8d777fbfc693cdf41fc8d56f049e9e91d40b954
Praveenvrao <vpraveenrao453@gmail.com>
2024-07-28 21:48:40 +0530

    java 1st code

---
+++ src/Package1/Java_Program1.java
@@ -0,0 +1,11 @@
+package Package1;
+
+public class Java_Program1 
+{
+	public static void main(String args[])
+	{
+		System.out.println("First program output..............");
+		System.out.println(10+333);
+		
+	}
+}
---
+++ src/Package2/Datatypes.java
@@ -0,0 +1,22 @@
+package Package2;
+
+public class Datatypes {
+
+	public static void main(String[] args) 
+	{
+		int a = 2;
+		int b=5,c=7,d=8;
+		int e,f;
+		e = 11;
+		f = 77;
+		String name = "Kamran";
+		System.out.println("The sum of a, b,c,d is "+(a+b+c+d) );
+		System.out.println("The sum of all integers is "+(a+b+c+d+e+f) );
+		System.out.println(name);
+		char grade = 'A';
+		String name2 = "Praveen";
+		System.out.println(name2 +" "+ grade);
+
+	}
+
+}
