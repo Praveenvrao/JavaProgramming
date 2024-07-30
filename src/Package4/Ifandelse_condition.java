58a24d933026734dbe14bbc671a04f912bf49d68
Praveenvrao <vpraveenrao453@gmail.com>
2024-07-30 13:18:11 +0530

    Control statements

---
+++ src/Package4/Ifandelse_condition.java
@@ -0,0 +1,30 @@
+package Package4;
+
+public class Ifandelse_condition {
+
+	public static void main(String[] args) {
+		// if 
+		int age = 3;
+		if (age >= 18) {
+			System.out.println("Eligible and age is "+age);
+		}
+		if (age >= 18) {
+			System.out.println("Eligible and age is "+age);
+		}
+		else
+		{
+			System.out.println("Not eligible and age is "+age);
+		}
+		// Odd or even
+		int a = 5;
+		if (a%2==0)
+		{
+			System.out.println("It's even number");
+		}
+		else {
+			System.out.println("It's an odd number");
+		}
+
+	}
+
+}
