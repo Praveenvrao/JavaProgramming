34898b7ba32e75efc3f97fcd0787c5ab212fabc1
Praveenvrao <vpraveenrao453@gmail.com>
2024-07-31 22:08:03 +0530

    While loop program

---
+++ src/Package5/Whileloop.java
@@ -0,0 +1,44 @@
+package Package5;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		// writing 1....10
+		/*int i = 1;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}*/
+		
+		//Writing even numbers aprch 1
+		/*int i =1;
+		while(i<=10)
+		{
+			if(i%2==0) {
+				System.out.println(i);
+			}
+			i++;
+		}*/
+		/*int i =1;
+		while(i<=10)
+		{
+			if(i%2==0) {
+				System.out.println(i + " Even number");
+			}
+			else {
+				System.out.println(i + " Odd number");
+			}
+			i++;
+		}*/
+		
+		int i = 1;
+		while(i<=10)
+		{
+			System.out.println("Program line number is " + i);
+			i++;
+		}
+
+	}
+
+}
