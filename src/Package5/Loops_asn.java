a497000d56eae1ef6485f9a7fb5b9ed64f41aeab
Praveenvrao <vpraveenrao453@gmail.com>
2024-08-01 22:17:46 +0530

    Loops asn2

--- src/Package5/Loops_asn.java
+++ src/Package5/Loops_asn.java
@@ -4,7 +4,7 @@ public class Loops_asn {
 
 	public static void main(String[] args) {
 		//1. Reverse a number  1234
-		int num = 12347635;
+		int num = 987656;
 		int rev = 0;
 		while(num != 0) {
 			rev = rev*10 + num%10;
@@ -28,6 +28,46 @@ public class Loops_asn {
 			System.out.println(original_number + " is not a palindrome number");
 		}
 		
+		// Counting number of digits in number
+		/*int k = 87654;
+		int count = 0;
+		while(k !=0) {
+			k = k/10;
+			count++;
+		}
+		System.out.println(count);*/
+		
+		//Counting even and odd digits in number
+		int j = 76543892;
+		int count = 0;
+		int even_count = 0;
+		int odd_count = 0;
+		
+		while(j!=0) {
+			j = j/10;
+			count++;
+			if (j%2==0) {
+				even_count++;
+			}
+			else {
+				odd_count++;
+			}
+		}
+		System.out.println("Total digits count of the number is "+ count);
+		System.out.println("Total even digits count of the number is "+ even_count);
+		System.out.println("Total digits count of the number is "+ odd_count);
+		
+		//Finding sum of the digits in number
+		
+		int l = 645300;
+		int sum = 0;
+		
+		while(l !=0) {
+			sum = sum+(l%10);
+			l/=10;
+		}
+		System.out.println("Sum of the number is - " + sum);
+
 	}
 
 }
