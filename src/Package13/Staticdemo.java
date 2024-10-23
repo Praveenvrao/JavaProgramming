ece6b6d5197270f8aa065d67f38917cd071b923a
Praveenvrao <vpraveenrao453@gmail.com>
2024-10-23 07:45:07 +0530

    Static and non static program

---
+++ src/Package13/Staticdemo.java
@@ -0,0 +1,24 @@
+package Package13;
+
+public class Staticdemo {
+	static int a = 4;
+	int b = 5;
+	
+	static void m1() {
+		System.out.println("this is static method");
+	}
+	void m2() {
+		System.out.println("this is non static mehod");
+	}
+	
+	/*public static void main(String[] args) {
+		System.out.println(a);
+		m1();
+		
+		Staticdemo obj1= new Staticdemo();
+		obj1.m2();
+		System.out.println(obj1.b);
+		
+	}*/
+
+}
---
+++ src/Package13/Staticdemomain.java
@@ -0,0 +1,16 @@
+package Package13;
+
+public class Staticdemomain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Staticdemo.a);
+		Staticdemo.m1();
+		
+		Staticdemo obj1= new Staticdemo();
+		obj1.m2();
+		System.out.println(obj1.b);
+
+	}
+
+}
