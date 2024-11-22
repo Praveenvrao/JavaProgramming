144f2f45f4748403740f72e5ae2f5e2f067c68bc
Praveenvrao <vpraveenrao453@gmail.com>
2024-11-22 23:07:38 +0530

    Collections

---
+++ src/Package20/Hashmapdemo.java
@@ -0,0 +1,18 @@
+package Package20;
+
+import java.util.HashMap;
+
+public class Hashmapdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//HashMap HM = new HashMap();
+		HashMap <Integer, String> HM = new HashMap<Integer, String>();
+
+		
+		
+
+	}
+
+}
