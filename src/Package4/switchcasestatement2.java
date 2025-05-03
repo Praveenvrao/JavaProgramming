9958a83b22d3b51f1cbf5dab23b2107d1af277be
Praveenvrao <vpraveenrao453@gmail.com>
2025-05-03 13:16:38 +0530

    conditional statements switch

---
+++ src/Package4/switchcasestatement2.java
@@ -0,0 +1,25 @@
+package Package4;
+
+public class switchcasestatement2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int teamrank = 9;
+		switch (teamrank) {
+		case 1 : System.out.println("MI"); break;
+		case 2 : System.out.println("GT"); break;
+		case 3 : System.out.println("RCB"); break;
+		case 4 : System.out.println("PBKS"); break;
+		case 5 : System.out.println("DC"); break;
+		case 6 : System.out.println("LSG"); break;
+		case 7 : System.out.println("KKR"); break;
+		case 8 : System.out.println("RR"); break;
+		case 9 : System.out.println("SRH"); break;
+		case 10 : System.out.println("CSK"); break;
+		default : System.out.println("Invalid input"); break;
+		}
+
+	}
+
+}
